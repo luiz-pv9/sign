674cda8e6b87c5139149cfc6d4e61b77af6870a7
Luiz Paulo Vasconcellos <luiz.pv9@gmail.com>
2017-02-15 11:35:21 -0200

    First commit

---
+++ lib/sign.ex
@@ -0,0 +1,18 @@
+defmodule Sign do
+  @moduledoc """
+  Documentation for Sign.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sign.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sign_test.exs
@@ -0,0 +1,8 @@
+defmodule SignTest do
+  use ExUnit.Case
+  doctest Sign
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
